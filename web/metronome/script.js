3d4cb32139ceecef9ff7fa09b414304cf220ad54
funnsam <funnsam8@gmail.com>
2025-12-27 16:22:17 +0000

    e

--- web/metronome/script.js
+++ web/metronome/script.js
@@ -12,16 +12,18 @@ start_btn.onclick = () => {
     clearInterval(timer);
     if (bpm_in.value < 0.1) return;
 
-    gainNode.gain.value = volume_in.value;
-
     var counter = 0;
 
     const interval = 60_000 / bpm_in.value;
     const beats = beats_in.value;
-    const flash = flash_in.checked;
 
     function tick() {
         const c = beats ? counter++ % beats : counter++;
+
+        gainNode.gain.value = volume_in.value;
+        if (flash_in.checked) {
+            root_elm.style.animation = (c ? "flash" : "flash_2") + " 0.1s linear";
+        }
 
         const oscillator = audioCtx.createOscillator();
         oscillator.type = "square";
@@ -30,10 +32,6 @@ start_btn.onclick = () => {
         oscillator.start();
 
         count_span.innerText = beats ? `${"█ ".repeat(c+1)}${"░ ".repeat(beats-c-1)}` : c + 1;
-
-        if (flash) {
-            root_elm.style.animation = (c ? "flash" : "flash_2") + " 0.1s linear";
-        }
 
         setTimeout(() => {
             oscillator.stop();
