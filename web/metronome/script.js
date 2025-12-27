820b717923a09f00541584032d3a8dae72cb3e9c
funnsam <funnsam8@gmail.com>
2025-12-27 15:15:08 +0000

    g

--- web/metronome/script.js
+++ web/metronome/script.js
@@ -5,6 +5,10 @@ const gainNode = audioCtx.createGain();
 gainNode.connect(audioCtx.destination);
 gainNode.gain.value = 0.15;
 
+root_elm.onanimationend = () => {
+    root_elm.style.animation = "none";
+};
+
 start_btn.onclick = () => {
     if (timer) clearInterval(timer);
     if (bpm_in.value < 0.1) return;
@@ -13,18 +17,30 @@ start_btn.onclick = () => {
 
     const interval = 60_000 / bpm_in.value;
     const beats = beats_in.value;
+    const flash = flash_in.checked;
 
-    timer = setInterval(() => {
+    function tick() {
+        const c = beats ? counter++ % beats : counter++;
+
         const oscillator = audioCtx.createOscillator();
         oscillator.type = "square";
-        oscillator.frequency.value = beats != 0 && counter % beats == 0 ? 783.99 : 523.25;
+        oscillator.frequency.value = c ? 523.25 : 783.99;
         oscillator.connect(gainNode);
         oscillator.start();
 
-        count.innerText = (beats != 0 ? counter++ % beats : counter) + 1;
+        count_span.innerText = beats ? `${"█ ".repeat(c+1)}${"░ ".repeat(beats-c-1)}` : c + 1;
 
-        setTimeout(() => oscillator.stop(), 50);
-    }, interval);
+        if (flash) {
+            root_elm.style.animation = (c ? "flash" : "flash_2") + " 0.1s linear";
+        }
+
+        setTimeout(() => {
+            oscillator.stop();
+        }, 50);
+    }
+
+    tick();
+    timer = setInterval(tick, interval);
 };
 
 stop_btn.onclick = () => {
