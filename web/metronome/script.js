291c2f6cb79259cdf1f7a27c11f6d34df0a6bf1d
funnsam <funnsam8@gmail.com>
2025-12-27 18:30:57 +0800

    h

--- web/metronome/script.js
+++ web/metronome/script.js
@@ -5,24 +5,28 @@ const gainNode = audioCtx.createGain();
 gainNode.connect(audioCtx.destination);
 gainNode.gain.value = 0.15;
 
-start.onclick = () => {
+start_btn.onclick = () => {
     if (timer) clearInterval(timer);
-    if (bpm.value < 0.1) return;
+    if (bpm_in.value < 0.1) return;
 
     var counter = 0;
 
-    const interval = 60_000 / bpm.value;
-    const b = beats.value;
+    const interval = 60_000 / bpm_in.value;
+    const beats = beats_in.value;
 
     timer = setInterval(() => {
         const oscillator = audioCtx.createOscillator();
         oscillator.type = "square";
-        oscillator.frequency.value = b != 0 && counter % b == 0 ? 783.99 : 523.25;
+        oscillator.frequency.value = beats != 0 && counter % beats == 0 ? 783.99 : 523.25;
         oscillator.connect(gainNode);
         oscillator.start();
 
-        count.innerText = (b != 0 ? counter++ % b : counter) + 1;
+        count.innerText = (beats != 0 ? counter++ % beats : counter) + 1;
 
         setTimeout(() => oscillator.stop(), 50);
     }, interval);
+};
+
+stop_btn.onclick = () => {
+    if (timer) clearInterval(timer);
 };
