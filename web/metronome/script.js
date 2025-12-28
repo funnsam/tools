09d9b2fa6fd1f45248266100bd55cc094c60d6e8
funnsam <funnsam8@gmail.com>
2025-12-28 08:14:12 +0000

    h

--- web/metronome/script.js
+++ web/metronome/script.js
@@ -1,47 +1,43 @@
-var timer;
+{
+    var timer;
 
-const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-const gainNode = audioCtx.createGain();
-gainNode.connect(audioCtx.destination);
+    const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+    const gainNode = audioCtx.createGain();
+    gainNode.connect(audioCtx.destination);
 
-root_elm.onanimationend = () => {
-    root_elm.style.animation = "none";
-};
+    root_elm.onanimationend = () => root_elm.style.animation = "none";
 
-start_btn.onclick = () => {
-    clearInterval(timer);
-    if (bpm_in.value < 0.1) return;
+    start_btn.onclick = () => {
+        clearInterval(timer);
+        if (bpm_in.value < 0.1) return;
 
-    var counter = 0;
+        var counter = 0;
 
-    const interval = 60_000 / bpm_in.value;
-    const beats = beats_in.value;
+        const interval = 60_000 / bpm_in.value;
+        const beats = beats_in.value;
 
-    function tick() {
-        const c = beats ? counter++ % beats : counter++;
+        function tick() {
+            const c = beats ? counter++ % beats : counter++;
 
-        gainNode.gain.value = volume_in.value;
-        if (flash_in.checked) {
-            root_elm.style.animation = (c ? "flash" : "flash_2") + " 0.1s linear";
+            gainNode.gain.value = volume_in.value;
+            if (flash_in.checked) {
+                root_elm.style.animation = (c ? "flash" : "flash_2") + " 0.1s linear";
+            }
+
+            const oscillator = audioCtx.createOscillator();
+            oscillator.type = "square";
+            oscillator.frequency.value = c ? 523.25 : 783.99;
+            oscillator.connect(gainNode);
+            oscillator.start();
+
+            count_span.innerText = beats ? `${"○ ".repeat(c)}●${" ○".repeat(beats-c-1)}` : c + 1;
+
+            setTimeout(() => oscillator.stop(), 50);
         }
 
-        const oscillator = audioCtx.createOscillator();
-        oscillator.type = "square";
-        oscillator.frequency.value = c ? 523.25 : 783.99;
-        oscillator.connect(gainNode);
-        oscillator.start();
+        tick();
+        timer = setInterval(tick, interval);
+    };
 
-        count_span.innerText = beats ? `${"○ ".repeat(c)}●${" ○".repeat(beats-c-1)}` : c + 1;
-
-        setTimeout(() => {
-            oscillator.stop();
-        }, 50);
-    }
-
-    tick();
-    timer = setInterval(tick, interval);
-};
-
-stop_btn.onclick = () => {
-    clearInterval(timer);
-};
+    stop_btn.onclick = () => clearInterval(timer);
+}
