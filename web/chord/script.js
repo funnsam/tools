7bddd8a4acc163ad6782e173dd6f130c1d9fc317
funnsam <funnsam8@gmail.com>
2026-03-11 21:12:48 +0800

    h

---
+++ web/chord/script.js
@@ -0,0 +1,88 @@
+{
+    const notes = [
+        "A", "A#", "B", "C", "C#", "D",
+        "D#", "E", "F", "F#", "G", "G#",
+    ];
+
+    const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+    const gainNode = audioCtx.createGain();
+    gainNode.connect(audioCtx.destination);
+
+    volume_in.oninput = () => gainNode.gain.value = volume_in.value;
+
+    notes.forEach((note, idx) => {
+        const btn = document.createElement("button");
+        btn.innerText = note;
+        btn.onclick = () => chord(idx, 0.5);
+
+        pitch_div.appendChild(btn);
+    });
+
+    relative_en.oninput = () => {
+        if (!relative_en.checked) {
+            relative_sel.innerHTML = "";
+            Array.from(pitch_div.children).forEach((btn, idx) => {
+                btn.innerText = notes[idx];
+            });
+            return;
+        }
+
+        notes.forEach((note, idx) => {
+            const btn = document.createElement("button");
+            btn.innerText = note;
+            btn.onclick = () => relative(idx);
+
+            relative_sel.appendChild(btn);
+        });
+    };
+    relative_en.oninput();
+
+    function relative(idx) {
+        Array.from(pitch_div.children).forEach((btn, j) => {
+            const d = (j + 12 - idx) % 12;
+            const rn = ["I", "#I", "II", "#II", "III", "IV", "#IV", "V", "#V", "VI", "#VI", "VII"];
+            btn.innerText = rn[d];
+        });
+    }
+
+    function playFrequency(freq, duration) {
+        const oscillator = audioCtx.createOscillator();
+        oscillator.type = "triangle";
+        oscillator.frequency.value = freq;
+        oscillator.connect(gainNode);
+        oscillator.start();
+        oscillator.stop(audioCtx.currentTime + duration);
+    }
+
+    // note is relative to A4
+    function frequencyOf(semitone) {
+        return 440 * Math.pow(2, semitone / 12 - 1);
+    }
+
+    function chord(semitone, duration) {
+        volume_in.oninput();
+
+        const intervals = {
+            "maj": [0, 4, 7],
+            "min": [0, 3, 7],
+            "dim": [0, 3, 6],
+            "aug": [0, 3, 8],
+            "sus4": [0, 5, 7],
+            "sus2": [0, 2, 7],
+            "5th": [0, 7],
+            "raw": [0],
+        }
+
+        intervals[type_in.value].forEach((st, _) => {
+            playFrequency(frequencyOf(semitone + st), duration);
+        })
+    }
+
+    // document.onkeydown = e => {
+    //     if (e.key == "ArrowDown" || e.key == "ArrowUp") {
+    //         e.preventDefault();
+    //         type_in.focus();
+    //         type_in.onkeydown(e);
+    //     }
+    // };
+}
